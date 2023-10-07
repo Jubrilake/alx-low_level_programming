cba29f09bbe3a987fb23a9418bf8b4f168cb37d0
Akerele Adebayo <jubrilake@gmail.com>
2023-10-06 19:50:47 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int firstNum, secondNum;
+
+	for (firstNum = '0'; firstNum < '9'; firstNum++)
+	{
+		for (secondNum = firstNum + 1; secondNum <= '9'; secondNum++)
+		{
+			if (firstNum != secondNum)
+			{
+				putchar(firstNum);
+				putchar(secondNum);
+				if (firstNum == '8' && secondNum == '9')
+				{
+					continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
