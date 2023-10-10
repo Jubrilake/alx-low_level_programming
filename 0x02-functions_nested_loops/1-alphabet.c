de78683bb1e01df8297068157cc59d09b8a89a77
Akerele Adebayo <jubrilake@gmail.com>
2023-10-10 09:31:12 -0700

    created 1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - the alphabet, in lowercase, followed by a new line.
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
