3ef49f7fd1e8d16922b17813c79bd9830af591b9
Akerele Adebayo <jubrilake@gmail.com>
2023-10-10 09:52:23 -0700

    created print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet ten times
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int times;
+	char alphabet;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+		return;
+	}
+}
