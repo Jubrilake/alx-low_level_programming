737e10c3f1fa06e846308486f749af36eb77765a
Akerele Adebayo <jubrilake@gmail.com>
2023-10-11 09:28:55 -0700

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,10 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-	for (num = 0; num <= n; num++)
-	{
+		for (num = 0; num <= n; num++)
+		{
 		_putchar('0');
-
+		
 		for (mult = 1; mult <= n; mult++)
 		{
 			_putchar(',');
@@ -33,17 +33,17 @@ void print_times_table(int n)
 					if (prod >= 100)
 					{
 						_putchar((prod / 100) + '0');
-						_putchar((prod / 100) % 10 + '0');
+						_putchar(((prod / 10)) % 10 + '0');
 					}
-					else if (prod <= 100 && prod >= 10)
+					else if (prod <= 99 && prod >= 10)
 					{
-						_putchar((prod / 100) + '0');
+						_putchar((prod / 10) + '0');
 					}
-					_putchar((prod % 100) + '0');
+					_putchar((prod % 10) + '0');
 				}
-			_putchar('_');
 			}
 		}
-	}
+		_putchar('\n');
+		}
 	}
 }
